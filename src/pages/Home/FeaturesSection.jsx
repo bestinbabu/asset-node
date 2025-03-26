fb0cc25a27ba31e07d5437f756e236295626c471
Vishnudath M <vishnuda333@gmail.com>
2025-03-26 17:09:16 +0530

    key features no

--- src/pages/Home/FeaturesSection.jsx
+++ src/pages/Home/FeaturesSection.jsx
@@ -4,13 +4,7 @@ import React from 'react';
 const FeaturesSection = () => {
   return (
     <section style={styles.features}>
-      <h3>Our Key Features</h3>
-      <ul>
-        <li>Real-time Asset Tracking</li>
-        <li>Predictive Maintenance Alerts</li>
-        <li>Immutable Asset History with Blockchain</li>
-        <li>Seamless HR Integration for Onboarding/Offboarding</li>
-      </ul>
+      
     </section>
   );
 };
